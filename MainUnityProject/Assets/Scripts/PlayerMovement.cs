28010c7c5ecd2603124545e896f100498ba61cf3
Programmering <teknisk+programmering-GDC2025-GR6@game.unf.dk>
2025-07-13 15:40:43 +0200

    kalder moving funktion på playersound

--- MainUnityProject/Assets/Scripts/PlayerMovement.cs
+++ MainUnityProject/Assets/Scripts/PlayerMovement.cs
@@ -8,19 +8,22 @@ public class PlayerMovement : MonoBehaviour
 
     public PlayerLook playerLook;
     public InputAction moveInput;
+    PlayerAudio playerAudio;
 
-    public CharacterController characterController;
+    CharacterController characterController;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveInput.Enable();
         characterController = GetComponent<CharacterController>();
+        playerAudio = GetComponent<PlayerAudio>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 moveDirection = moveInput.ReadValue<Vector2>();
+        if (moveDirection != Vector2.zero) playerAudio.Moving();
         Vector3 lookDirection = playerLook.lookDirection2D;
         Vector3 sideDirection = Vector3.Cross(lookDirection, Vector3.up);
         characterController.Move(speed * moveDirection.y * Time.deltaTime * lookDirection - speed * moveDirection.x * Time.deltaTime * sideDirection);
